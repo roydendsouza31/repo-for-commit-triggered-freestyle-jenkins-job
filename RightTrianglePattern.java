ffd7467b788df07aad394f3dd58d370fd5f495a5
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-07-31 17:37:18 +0530

    Update RightTrianglePattern.java - set rows = 6

--- RightTrianglePattern.java
+++ RightTrianglePattern.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {   
 //i for rows and j for columns      
 //row denotes the number of rows you want to print  
-int i, j, row=12;   
+int i, j, row=6;   
 //outer loop for rows  
 for(i=0; i<row; i++)   
 {   
