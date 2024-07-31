3f85b6f7c48b757216238ab2d4213ee2fcf93706
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-07-31 17:51:07 +0530

    Update RightTrianglePattern.java - Symbol changed to $

--- RightTrianglePattern.java
+++ RightTrianglePattern.java
@@ -12,7 +12,7 @@ for(i=0; i<row; i++)
 for(j=0; j<=i; j++)   
 {   
 //prints stars   
-System.out.print("* ");   
+System.out.print("$ ");   
 }   
 //throws the cursor in a new line after printing each line  
 System.out.println();   
